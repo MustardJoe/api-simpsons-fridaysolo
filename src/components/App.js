0887311dab831a459be70231e6e6dae53b4e0855
Jonathan Forney <jonathan.forney@gmail.com>
2019-05-31 23:24:04 -0700

    wrote api call in app, data now populates page

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import Component from './Component.js';
 import Header from './Header.js';
+import QuoteList from './QuoteList.js';
 import Footer from './Footer.js';
 import quotesApi from '../services/simpsons-api.js';
+import hashStorage from '../hash-storage.js';
 
 class App extends Component {
     render() {
@@ -11,6 +13,17 @@ class App extends Component {
 
         const header = new Header();
         dom.insertBefore(header.render(), main);
+
+        const quoteList = new QuoteList({ quotes: [] });
+        main.appendChild(quoteList.render());
+
+        // const queryProps = hashStorage.get();
+        // API CALL occurs here
+        quotesApi.getQuotes()
+            .then(response => {
+                let quotes = response;
+                quoteList.update({ quotes });
+            });
 
         const footer = new Footer();
         main.appendChild(footer.render());
