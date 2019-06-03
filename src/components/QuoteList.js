79a884be504805d504a485dd9714aa69fff984ed
Jonathan Forney <jonathan.forney@gmail.com>
2019-06-02 19:43:21 -0700

    style changes and removed static data from page

--- src/components/QuoteList.js
+++ src/components/QuoteList.js
@@ -18,13 +18,7 @@ class QuoteList extends Component {
     renderTemplate() {
         return /*html*/`
         <ul class="quote-list">
-            <li class="quote-item">
-                <h2>Homer Simpson</h2>
-                <img src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939">
-                <blockquote>
-                    I believe the children are the future... Unless we stop them now!
-                </blockquote>
-            </li>
+            
         </ul>
     `;
     }
--- test/QuoteItem.test.js
+++ test/QuoteItem.test.js
@@ -16,26 +16,17 @@ test('create quote item DOM', assert => {
     const quoteItem = new QuoteItem({ quote });
     const rendered = quoteItem.renderTemplate();
 
-    // const expected = /*html*/`
-    //     <li class="quote-item">
-    //         <h1>Homer Simpson</h1>
-    //         <img src="https://res.cloudinary.com/dzxqhkyqd/image/fetch/c_scale,w_500/https://res.cloudinary.com/dzxqhkyqd/image/upload/v1552429540/bender.png" alt="Bender">
-    //         <blockquote>
-    //             Bite my shiny metal ass.
-    //         </blockquote>
-    //     </li>
-    // `;
-    //console.log(expected);
+    const expected = /*html*/`
+        <li class="quote-item">
+            <h2>Homer Simpson</h2>
+            <img src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939">
+            <blockquote class="quote-quote">
+                I believe the children are the future... Unless we stop them now!
+            </blockquote>
+        </li>
+    `;
 
     //assert
-    assert.htmlEqual(rendered, /*html*/`
-            <li class="quote-item">
-                <h1>Homer Simpson</h1>
-                <img src="https://cdn.glitch.com/3c3ffadc-3406-4440-bb95-d40ec8fcde72%2FHomerSimpson.png?1497567511939">
-                <blockquote>
-                    I believe the children are the future... Unless we stop them now!
-                </blockquote>
-            </li>
-    `);
+    assert.htmlEqual(rendered, expected);
 });
 
